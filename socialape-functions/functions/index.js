4efa2654603552a1bfa5d328ed685c3903e38b57
Bendeguz Szucs <benszucs@gmail.com>
2020-01-06 12:24:07 +0000

    create login route and validations

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -54,6 +54,17 @@ app.post('/scream', (req, res) => {
 		});
 });
 
+const isEmail = email => {
+	const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (email.match(regEx)) return true;
+	else return false;
+};
+
+const isEmpty = string => {
+	if (string.trim() === '') return true;
+	else return false;
+};
+
 // Signup route
 app.post('/signup', (req, res) => {
 	const newUser = {
@@ -63,9 +74,25 @@ app.post('/signup', (req, res) => {
 		handle: req.body.handle,
 	};
 
-	let token, userId;
+	// Validations
+	let errors = {};
+	// Email
+	if (isEmpty(newUser.email)) {
+		errors.email = 'Must must not be empty.';
+	} else if (!isEmail(newUser.email)) {
+		errors.email = 'Must be a valid email address.';
+	}
+	// Password
+	if (isEmpty(newUser.password)) errors.password = 'Must not be empty.';
+	if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match.';
+	// Handle
+	if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty.';
+
+	if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
 	// TODO validate data
+	let token, userId;
+
 	db.doc(`/users/${newUser.handle}`)
 		.get()
 		.then(doc => {
@@ -99,6 +126,35 @@ app.post('/signup', (req, res) => {
 			} else {
 				return res.status(500).json({ error: err.code });
 			}
+		});
+});
+
+app.post('/login', (req, res) => {
+	const user = {
+		email: req.body.email,
+		password: req.body.password,
+	};
+
+	let errors = {};
+
+	if (isEmpty(user.email)) errors.email = 'Must not be empty.';
+	if (isEmpty(user.password)) errors.password = 'Must not be empty.';
+	if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+	firebase
+		.auth()
+		.signInWithEmailAndPassword(user.email, user.password)
+		.then(data => {
+			return data.user.getIdToken();
+		})
+		.then(token => {
+			return res.json({ token });
+		})
+		.catch(err => {
+			console.error(err);
+			if (err.code === 'auth/wrong-password')
+				return res.status(403).json({ general: 'Wrong credential, please try again.' });
+			return res.status(500).json({ error: err.code });
 		});
 });
 
