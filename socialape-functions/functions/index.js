7530f62c6fb7489d5099b0bbef57eaa9d1f5a50d
Bendeguz Szucs <benszucs@gmail.com>
2020-01-09 13:00:16 +0000

    create like, unlike and delete scream routes

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -2,16 +2,24 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
+const {
+	getAllScreams,
+	postOneScream,
+	getScream,
+	commentOnScream,
+	likeScream,
+	unlikeScream,
+	deleteScream,
+} = require('./handlers/screams');
 const { signUp, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
 // Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
-// TODO: delete scream
-// TODO: like scream
-// TODO: unlike scream
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 // Users routes
